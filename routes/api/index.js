5a1dbf11813c414c3f56479e16959fe8c712e593
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-05-05 20:53:16 -0700

    removed product tag post route

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,11 +5,9 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
-const productTagRoutes = require('./productTag-routes');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
-router.use('/producttags', productTagRoutes);
 
 module.exports = router;
