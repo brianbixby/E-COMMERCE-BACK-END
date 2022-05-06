0d46741d5322d684a01bcfea7a8669e72016dc06
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-05-05 21:10:08 -0700

    updated delete cascade

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, { foreignKey: 'category_id' });
+Product.belongsTo(Category, { foreignKey: 'category_id', onDelete: 'CASCADE' });
 
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: 'category_id' });
