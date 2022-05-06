bcc0b4e22330fe39025a73eb55e5f3dbb93f2242
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-05-05 17:33:58 -0700

    fixed the duplicate foreign key issue by specifying a foreign key in my association

--- models/index.js
+++ models/index.js
@@ -6,9 +6,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, { foreignKey: 'category_id' });
+// Product.belongsTo(Category);
+
+
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, { foreignKey: 'category_id' });
+// Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
